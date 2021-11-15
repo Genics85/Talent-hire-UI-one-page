be9bc464b1f8f88a8f206a540f831e58ef35a4fc
Genics85 <eugeneamo85@gmail.com>
2021-11-15 14:48:54 +0000

    about to do the profiles

--- lib/main.dart
+++ lib/main.dart
@@ -60,12 +60,85 @@ class _MyHomePageState extends State<MyHomePage> {
           Stack(
             children:<Widget>[
               Container(
+                alignment: Alignment.center,
                 height:100,
                 color:Colors.white,
                 child:Text("Get Coaching", style:TextStyle(fontFamily:'Montserrat', fontSize:20)),
+              ),
+              Container(
+                alignment:Alignment.center,
+                
+
+                margin: EdgeInsets.fromLTRB(25,80,25,0),
+                padding: EdgeInsets.all(20),
+
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                  boxShadow:[
+                    BoxShadow(blurRadius: 5,offset: Offset(0,3),
+                    color: Colors.grey.withOpacity(.08), spreadRadius: 5)
+                  ]
+                ),
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Column(
+                          children: [
+                            Container(
+                              alignment:Alignment.topLeft,
+                              child: Text("YOU HAVE", style:TextStyle(color: Colors.grey)),
+                            ),
+
+
+                            Row(
+                              children: [
+                                Text("206",style: TextStyle(
+                                    color: Colors.black,fontSize:40,fontWeight: FontWeight.bold
+                                ),),
+                                Icon(Icons.favorite,color: Colors.red,)
+                              ],
+                            ),
+                          ],
+                        ),
+
+
+                        SizedBox(width: 105),
+                        Container(
+                          width: 100,
+                          height: 45,
+                          decoration: BoxDecoration(
+                            color: Colors.greenAccent.withOpacity(0.2),borderRadius: BorderRadius.circular(10)
+                          ),
+                          child: Center(child:Text("Buy More", style: TextStyle(
+                            color:Colors.green,fontSize: 16, fontWeight: FontWeight.bold
+                          ),) ,),
+                        )
+
+                      ],
+
+                    )
+                  ]
+                ),
+
               )
             ]
           ),
+          SizedBox(height: 40,),
+          Container(
+            margin: EdgeInsets.only(left:20,right:20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("My Coaches",
+                style:TextStyle(color:Colors.grey,fontWeight: FontWeight.bold)),
+                Text("View Past Sessions",
+                style:TextStyle(color:Colors.green,fontWeight: FontWeight.bold))
+              ],
+            ),
+
+          )
 
         ]
       ),
